47ed4a072519912e376b51ecd5a5b8c53d641578
Luis <luis3mendez6@gmail.com>
2024-06-04 07:36:37 -0600

    Cambios sincronizados

---
+++ icca_dev/icca_analysis/icca_simple_report.py
@@ -0,0 +1,14 @@
+""" ICCA simple report to perfor comparation between MES STAR and ICCA produciton count
+
+    Perform a Total count by shifts range
+    Perform a comparassion of how many serials in ICCA there is in MES
+    Perform a comparassion of how many serials IN MES there is in ICCA
+"""
+import datetime
+import dateparser
+
+def report_total_shift(date_str : str):
+    """Return a preint output to report total count by shift"""
+
+    return
+
